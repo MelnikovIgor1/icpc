6e90d746a2ddbeab5ad8aeefa5a77ffbb43dc64c
jolex007 <jokers3000@gmail.com>
2020-11-15 10:44:13 +0300

    Added some new files

---
+++ olymp_C/olymp_C/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+#include <stack>
+
+using ll = long long;
+using ld = long double;
+
+using namespace std;
+
+void solve() {
+    
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+    
+    return 0;
+}
